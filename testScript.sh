34a653852c9aa77762e1444fccb25726edebbef6
Will Taylor <willtaylor116@gmail.com>
2020-10-10 18:56:38 -0500

    Added testing script, data files

---
+++ testScript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python source.py synthetic-1.csv 1
+python source.py synthetic-1.csv 2
+python source.py synthetic-1.csv 4
+python source.py synthetic-1.csv 7
+python source.py synthetic-2.csv 1
+python source.py synthetic-2.csv 2
+python source.py synthetic-2.csv 4
+python source.py synthetic-2.csv 7
+python source.py synthetic-3.csv 1
+python source.py synthetic-3.csv 2
+python source.py synthetic-3.csv 4
+python source.py synthetic-3.csv 7
